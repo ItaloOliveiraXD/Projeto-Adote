c8d29b48353043fe4ca7a4c7b5176aef84d64184
Italo Oliveira <italooliveira-xd@hotmail.com>
2023-01-13 01:17:03 -0300

    Criando a tabela Pet no banco de dados pela models.

--- divulgar/models.py
+++ divulgar/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Raca(models.Model):
@@ -13,3 +14,24 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.tag
+
+
+class Pet(models.Model):
+    choice_status = (
+        ('P', 'Para Adoção'),
+        ('A', 'Adotado')
+    )
+
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    foto = models.ImageField(upload_to='fotos_pet')
+    nome = models.CharField(max_length=100)
+    descicao = models.TextField()
+    estado = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=50)
+    tag = models.ManyToManyField(Tag)
+    raca = models.ForeignKey(Raca, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=1, choices=choice_status)
+
+    def __str__(self):
+        return self.nome
