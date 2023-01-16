7dbe487244501ccda55e2f6a91efad22a0e2b358
Italo Oliveira <italooliveira-xd@hotmail.com>
2023-01-15 23:21:51 -0300

    Colocando o decorador login_required.

--- divulgar/views.py
+++ divulgar/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def novo_pet(request):
     if request.method == 'GET':
         return render(request, 'divulgar/novo_pet.html')
